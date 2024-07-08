78ce5794600eafce18fd3e6e99731581199fe504
Domenico Ferraro <ferraro.domenico125@gmail.com>
2024-07-08 12:50:39 +0200

    feat: test I/O bound operations

--- notebook/pullnotebook.sh
+++ notebook/pullnotebook.sh
@@ -1,2 +1,3 @@
 docker cp pythontest:/app/notebook/pipeline/plot.ipynb ./notebook/pipeline/plot.ipynb
 docker cp pythontest:/app/notebook/farm/plot.ipynb ./notebook/farm/plot.ipynb
+docker cp pythontest:/app/notebook/farm/plot_io_bound.ipynb ./notebook/farm/plot_io_bound.ipynb
